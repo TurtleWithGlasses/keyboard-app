a20793803a7b14bf3f7f5065fbbe2c65e3fd2aa5
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-08-07 16:31:59 +0300

    Create select function to give function to each button to print value Add window title

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 keyboard_app = tk.Tk()
+keyboard_app.title("Practical Keyboard")
 
 keys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "=",
         "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "DEL",
@@ -16,3 +17,50 @@ entry.grid(row=0, columnspan=15)
 var_row = 1
 var_column = 0
 
+def select(value, x, y):
+        if current_button != [-1, -1]:
+                button_L[current_button[0]][current_button[1]].configure(highlightbackground="red")
+                button_L[current_button[0]][current_button[1]].configure(highlightcolor="red")
+        current_button[:] = [x, y]
+        button_L[x][y].configure(highlightbackground="red")
+        button_L[x][y].configure(highlightcolor="red")
+
+        if value == "DEL":
+                input_value = entry.get("1.0", "end-2c")
+                entry.delete("1.0", "end")
+                entry.insert("1.0", input_value, "end")
+        elif value == "SPACE":
+                entry.insert("insert", " ")
+        elif value == "TAB":
+                entry.insert("insert", "    ")
+        else:
+                entry.insert("end", value)
+
+
+for button in keys:
+        if button != "SPACE":
+                but = tk.Button(keyboard_app, text=button, width=5, bg="gray", fg="black",
+                                highlightthickness=4, activebackground="light gray", activeforeground="dark gray", highlightcolor="red", relief="raised", padx=12, pady=4, bd=4, command=lambda x=button, i=var_row-1, j=var_column: select(x,i,j))
+                but.bind("<Return>", lambda event, x=button, i=var_row-1,j=var_column: select(x,i,j))
+                button_L[var_row-1].append(but)
+                but.grid(row=var_row, column=var_column)
+
+
+        if button == "SPACE":
+            but = tk.Button(keyboard_app, text=button, width=60, bg="gray", fg="black",
+                            highlightthickness=4, activebackground="light gray", activeforeground="dark gray",
+                            highlightcolor="red", relief="raised", padx=4, pady=4, bd=4,
+                            command=lambda x=button, i=var_row - 1,
+                                           j=var_column: select(x, i, j))
+            but.bind("<Return>", lambda event, x=button, i=var_row - 1, j=var_column: select(x, i, j))
+            button_L[var_row - 1].append(but)
+            but.grid(row=6, columnspan=16)
+
+        var_column += 1
+        if var_column > 10:
+            var_column = 0
+            var_row += 1
+            button_L.append([])
+
+
+keyboard_app.mainloop()
